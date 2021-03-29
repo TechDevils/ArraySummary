b59cea6a2f5a3997a2078512f5c8228f6c994c1a
Andrew Chadwick <chadders13@googlemail.com>
2021-03-29 23:49:26 +0100

    Update files

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+var summary = require("summary");
+
 function ArraySummary() {
     function processData(name, numbers) {
+        var data = summary(numbers, true);
         let output = {
             name: name,
             min: undefined,
@@ -12,31 +15,16 @@ function ArraySummary() {
             total: undefined
         }
 
-        numbers = numbers.sort((a, b) => {
-            return b - a
-        });
-
-        output.total = getTotal(numbers);
-
-        output.count = numbers.length;
-        var minMax = getMinAndMaxFromInts(numbers);
-        output.max = minMax.max;
-        output.min = minMax.min;
-        output.range = output.max - output.min;
-        if (isNaN(output.range)) {
-            output.range = undefined;
-        }
-        var mean = getAverage(numbers);
-        output.mean = mean;
-
-        var quartiles = getQuartiles(numbers);
-        output.median = quartiles.median;
-        output.q1 = quartiles.q1;
-        output.q3 = quartiles.q3;
-
-        var mode = getMostCommon(numbers);
-        output.mode = mode.top;
-        output.modeValues = mode.allValues;
+        output.max = data.max();
+        output.min = data.min();
+        output.mean = data.mean();
+        output.median = data.median();
+        output.mode = data.mode();
+        output.standardDeviation = data.sd();
+        output.quartile = data.quartile();
+        output.size = data.size();
+        output.total = data.sum();
+        output.variance = data.variance();
 
         var outputKeys = Object.keys(output);
 
@@ -48,74 +36,6 @@ function ArraySummary() {
         }
 
         return output;
-    }
-
-    function getMinAndMaxFromInts(input) {
-        var output = {
-            max: input[0],
-            min: input[input.length - 1]
-        };
-        return output;
-    }
-
-    function getAverage(input) {
-        var total = input.reduce((a, cv) => a + cv);
-        var output = total / input.length;
-        if (isNaN(output)) {
-            return undefined;
-        }
-        return output;
-    }
-
-    function getQuartiles(input) {
-        var middleArrayValue = Math.floor(input.length / 2);
-        var output = {};
-        if (input.length % 2 == 0) {
-            var leftMiddle = input[middleArrayValue - 1];
-            var rightMiddle = input[middleArrayValue];
-            output.median = (leftMiddle + rightMiddle) / 2;
-        } else {
-            output.median = input[middleArrayValue];
-        }
-        return output;
-    }
-
-    function getMostCommon(input) {
-        var values = {};
-        for (let i = 0; i < input.length; i++) {
-            const number = input[i];
-            if (values[number]) {
-                values[number] += 1;
-            } else {
-                values[number] = {};
-                values[number] = 1;
-            }
-        }
-        var keys = Object.keys(values);
-        var top = undefined;
-        var maxCount = 0;
-        for (let k = 0; k < keys.length; k++) {
-            let key = keys[k];
-            const checkValue = values[key];
-            if (checkValue == 1) {
-                continue;
-            }
-            if (checkValue > maxCount) {
-                top = key;
-                maxCount = checkValue;
-            }
-        }
-        var output = {};
-        output["allValues"] = values;
-        output["top"] = top;
-
-        return output;
-    }
-
-    function getTotal(input) {
-        return input.reduce((p, c, i) => {
-            return p += c;
-        })
     }
 
     return {
